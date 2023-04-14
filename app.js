db3e499d6b044f7ae4cd2459a8642f4ecafdf495
adrianomsjf <adrianomsjf@gmail.com>
2023-04-14 08:18:34 -0300

    Adcionado Cors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 require('dotenv').config()
+const cors = require('cors');
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World! ==>'+process.env.teste)
